1df51764c90a6b67025964fcca94bbf90a594065
unknown <carlolim543@gmail.com>
2022-11-19 13:29:55 -0800

    Digital Clock (Adding Seconds, Minutes, Hours and Am/Pm)

--- digital_clock.py
+++ digital_clock.py
@@ -3,7 +3,17 @@ import time
 
 window = ui.Tk()
 
+def update_clock():
+    hours = time.strftime("%I")
+    minutes = time.strftime("%M")
+    seconds = time.strftime("%S")
+    am_or_pm = time.strftime("%p")
+    time_text = hours + ":" + minutes + ":" + seconds + " " + am_or_pm
+    digital_clock_lbl.config(text=time_text)
+
 digital_clock_lbl = ui.Label(window, text="00:00:00", font="Helvetica 72 bold")
 digital_clock_lbl.pack()
+
+update_clock()
 
 window.mainloop()
