4478a994b28aeffcbc469b45dae6ea80922e9b26
unknown <carlolim543@gmail.com>
2022-11-19 14:03:21 -0800

    Analog Clock (Adding Analog Clock Hands)

--- analog_clock.py
+++ analog_clock.py
@@ -17,6 +17,19 @@ canvas.pack(expand=True, fill='both')
 bg = ui.PhotoImage(file='dial_400.png')
 canvas.create_image(200, 200, image=bg)
 
+center_x = 200
+center_y = 200
+
+seconds_hand_len = 95
+minutes_hand_len = 80
+hours_hand_len = 60
+
+seconds_hand = canvas.create_line(200, 200, 200 + seconds_hand_len, 200 + seconds_hand_len, width=1.5, fill='red')
+
+minutes_hand = canvas.create_line(200, 200, 200 + minutes_hand_len, 200 + minutes_hand_len, width=2, fill='white')
+
+hours_hand = canvas.create_line(200, 200, 200 + hours_hand_len, 200 + hours_hand_len, width=4, fill='white')
+
 update_clock()
 
 window.mainloop()
