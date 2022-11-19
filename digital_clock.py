aef7da2811a0f8a8d4b59325414e1c75e694f4cb
unknown <carlolim543@gmail.com>
2022-11-19 13:25:49 -0800

    Digital Clock (Creating Digital Clock Structure)

---
+++ digital_clock.py
@@ -0,0 +1,9 @@
+import tkinter as ui
+import time
+
+window = ui.Tk()
+
+digital_clock_lbl = ui.Label(window, text="00:00:00", font="Helvetica 72 bold")
+digital_clock_lbl.pack()
+
+window.mainloop()
