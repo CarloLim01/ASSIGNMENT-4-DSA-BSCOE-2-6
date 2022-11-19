ea3cdc4390b8e4da44ba452bc081f481c2418f3d
unknown <carlolim543@gmail.com>
2022-11-19 14:14:21 -0800

    Analog Clock (Finalizing Analog Clock)

--- analog_clock.py
+++ analog_clock.py
@@ -11,6 +11,22 @@ def update_clock():
     minutes = int(time.strftime("%M"))
     seconds = int(time.strftime("%S"))
 
+    seconds_x = seconds_hand_len * math.sin(math.radians(seconds * 6)) + center_x
+    seconds_y = -1 * seconds_hand_len * math.cos(math.radians(seconds * 6)) + center_y
+    canvas.coords(seconds_hand, center_x, center_y, seconds_x, seconds_y)
+
+    # updating minutes hand
+    minutes_x = minutes_hand_len * math.sin(math.radians(minutes * 6)) + center_x
+    minutes_y = -1 * minutes_hand_len * math.cos(math.radians(minutes * 6)) + center_y
+    canvas.coords(minutes_hand, center_x, center_y, minutes_x, minutes_y)
+
+    # updating hours hand
+    hours_x = hours_hand_len * math.sin(math.radians(hours * 30 + 0.5 * minutes + 0.008 * seconds)) + center_x
+    hours_y = -1 * hours_hand_len * math.cos(math.radians(hours * 30 + 0.5 * minutes + 0.008 * seconds)) + center_y
+    canvas.coords(hours_hand, center_x, center_y, hours_x, hours_y)
+
+    window.after(1000, update_clock)
+
 canvas = ui.Canvas(window, width=400, height=400, bg="black")
 canvas.pack(expand=True, fill='both')
 
